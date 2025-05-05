f22966794d451b9e8f41bb2d6c0e1cd4ccc3ee1d
fuurin0111 <159630653+fuurin0111@users.noreply.github.com>
2025-05-05 23:31:33 +0900

    ヘッダー作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import Header from './componet/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl mb-4">Welcome to ガチャサイト！</h1>
+        {/* 他のコンポーネントやページ内容 */}
+      </main>
     </div>
   );
 }
